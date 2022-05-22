7cbde152224bfe2a0f4144ea3885e2a6f74a7720
chenbigdavid <jclwan5200@gmail.com>
2022-05-22 21:07:19 +0800

    "change the method of missed_states"

--- day25/us-states-game-start-1/main.py
+++ day25/us-states-game-start-1/main.py
@@ -44,10 +44,7 @@ while len(guessed_states) < 50:
     # 退出的时候 保留未猜测的州信息
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         pandas.DataFrame(missing_states).to_csv("remain_to_record.csv")
         break
 
