e7811048b1c00ae6f00eeada0eb70e5c6fd4dad9
chenbigdavid <jclwan5200@gmail.com>
2022-05-22 23:00:52 +0800

    "complete nato project"

---
+++ day26/NATO_project/main.py
@@ -0,0 +1,21 @@
+# 1 transfer dataFrame to dict
+
+import pandas
+nato_data = pandas.read_csv("nato.csv")
+nato_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
+# print(nato_dict)
+
+# 2 accept the user input(letter) and tell them the code
+ask = input("what do you say: ").upper()
+# 根据key 找到值  在转换成list
+ask_list = [letter for letter in ask]
+# print(ask_list)
+# result = []
+# for letter in ask_list:
+#     for (key, value) in nato_dict.items():
+#         if letter == key:
+#             result.append(value)
+
+# result = [code for word in ask_list for (letter, code) in nato_dict.items() if word == letter]
+result = [nato_dict[letter] for letter in ask_list]
+print(result)
